3e0beddf1c9803b0a6ce28a7d3681d1a653df9b6
Edward Brook <31439728+Edbtvplays@users.noreply.github.com>
2020-12-26 22:56:13 +0000

    Changed Admin Prefix To Account Nav

--- resources/views/layouts/nav/account.blade.php
+++ resources/views/layouts/nav/account.blade.php
@@ -4,6 +4,6 @@
             ->addItemClass('py-1 block text-sm text-gray-800 px-2 py-1')
             ->add(\Spatie\Menu\Laravel\Html::raw('<h3>Account</h3>')->addParentClass('px-2 text-gray-500 uppercase tracking-wide font-semibold text-xs mt-6 pb-1'))
             ->route('admin.account.edit', 'Profile')
-            ->setActiveFromRequest(config('blog.prefix'))
+            ->setActiveFromRequest(config('blog.adminprefix'))
     }}
 </nav>
